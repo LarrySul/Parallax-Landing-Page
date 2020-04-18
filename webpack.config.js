7951bb667e29ef8b2af17e17333b704cc329c901
larrysul <suleabimbola@gmail.com>
2020-04-18 03:34:53 +0100

    added HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/js/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve('./build')
     },
     module: {
         rules: [
@@ -42,5 +45,14 @@ module.exports = {
                 ]
             },
         ]
-    }
+    },
+    plugins: [
+        // New plugin
+        new HtmlWebpackPlugin({
+            // injects bundle.js to our new index.html
+            inject: true,
+            // copys the content of the existing index.html to the new /build index.html
+            template: path.resolve('./index.html'),
+        }),
+    ]
 }
